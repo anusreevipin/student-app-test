67134cf3c70748e47e8f4bdb166aecf668bb21c9
Anusree <anu.sudhakaran91@gmail.com>
2020-11-27 21:01:48 +0530

    created migrations for all database tables

---
+++ database/migrations/2020_11_27_152142_create_student_scores_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_scores', function (Blueprint $table) {
+            $table->id();
+            $table->integer('student_id');
+            $table->integer('course_id');
+            $table->double('score', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_scores');
+    }
+}
